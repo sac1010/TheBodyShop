902bc0558fecfdf95014304d655776b558648832
Mayur7038 <96098887+Mayur7038@users.noreply.github.com>
2021-12-18 19:21:28 +0530

    updated

--- mainwebpage/procced.js
+++ mainwebpage/procced.js
@@ -28,26 +28,13 @@ var obj = JSON.parse(localStorage.getItem("data")) || {};
 
 var cartData = JSON.parse(localStorage.getItem("cartData")) || []; 
 
-cartData.map(function (elem){
-    var div = document.createElement("div");
-    var image = document.createElement("img");
-    image.setAttribute("src",elem.image)
-    image.setAttribute("class","imagesDo");
-    var name = document.createElement("h4");
-    name.textContent = elem.name;
-    var price = document.createElement("h4");
-    price.textContent = "₹"+elem.price;
-
-    div.append(image,name,price)
-
-    document.querySelector("#showDetails").append(div)
-
-})
 
 
-displayTotal ()
+mapping();
 
-function displayTotal(){
+function mapping(){
+
+    document.querySelector("#showDetails").innerHTML = "";
 
     var total = 0;
 
@@ -55,12 +42,57 @@ function displayTotal(){
         total = Number(cartData[i].price) + total +100; 
     }
     
-    total = "Total Purchased +" + "Delivery Charges = " + total;
+    total = "Toal Purchase : " + total;
     
     
-    var h1 = document.createElement("h4");
+    var h1 = document.createElement("h3");
+    h1.setAttribute("id","total")
     
     h1.textContent = total;
     
-    document.querySelector("#showDetails").append(h1)   
+    document.querySelector("#showDetails").append(h1);
+
+    cartData.map(function (elem,index){
+        
+        var maindiv =  document.createElement("div");
+        maindiv.setAttribute("id","maindiv");
+
+        var div1 = document.createElement("div");
+        div1.setAttribute("id","side")
+        var btn = document.createElement("button");
+        btn.setAttribute("id","remove")
+        btn.textContent =  "x" ;
+        btn.addEventListener("click",function(){
+            deletask(index)
+        })
+        var image = document.createElement("img");
+        image.setAttribute("id","cartImg")
+        image.setAttribute("src",elem.image);
+        div1.append(btn,image)
+
+
+        var div2 = document.createElement("div");
+        var name = document.createElement("h4");
+        name.textContent = elem.name;
+        var price = document.createElement("h4");
+        price.textContent = "₹"+elem.price;
+        div2.append(name,price)
+    
+        maindiv.append(div1,div2)
+    
+        document.querySelector("#showDetails").append(maindiv)
+    
+    })
+    
+
 }
+
+
+
+function deletask(index){
+    cartData.splice(index,1)
+    localStorage.setItem("cartData", JSON.stringify(cartData));
+    mapping();
+
+}
+
